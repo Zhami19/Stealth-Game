3c829234678ec11fec88b95995b0eb20184c6420
Zhamanta Chavez <zhamantach04@gmail.com>
2025-09-13 15:03:00 -0500

    "Finalized" Gizmos, PlayerMovement, and Progress on Navigation

---
+++ Assets/Scripts/LineOfSight.cs
@@ -0,0 +1,65 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class LineOfSight : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Vector3 directionToTarget;
+    Vector3 forwardDirection;
+    float dot;
+
+    [SerializeField] float viewDistance = 5f;
+    [SerializeField, Range(-1f, 1f)] float ordinate = .5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Gizmos.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        directionToTarget = (target.position - transform.position).normalized;
+        forwardDirection = transform.forward;
+
+        dot = Vector3.Dot(forwardDirection, directionToTarget);
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+        if ((dot > ordinate) && (distanceToTarget <= viewDistance))
+            Debug.Log("He is in front");
+
+        if ((dot < -ordinate) && (distanceToTarget <= viewDistance))
+            Debug.Log("He is behind");
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Calculate viewAngle from ordinate
+        float viewAngle = Mathf.Acos(ordinate) * Mathf.Rad2Deg;
+
+        // Draw range circle
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
+
+        // Unit forward vector
+        Vector3 forward = transform.forward;
+
+        // Rotate first
+        Vector3 leftDir = Quaternion.Euler(0, -viewAngle, 0) * forward;
+        Vector3 rightDir = Quaternion.Euler(0, viewAngle, 0) * forward;
+
+        // Then scale by viewDistance
+        leftDir *= viewDistance;
+        rightDir *= viewDistance;
+        Vector3 centerDir = forward * viewDistance;
+
+        // Draw lines
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + leftDir);
+        Gizmos.DrawLine(transform.position, transform.position + rightDir);
+        Gizmos.DrawLine(transform.position, transform.position + centerDir);
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // movement values
+    [Header("Movement Values")]
+    [SerializeField] float moveSpeed = 1.0f;
+
+    // references
+    Rigidbody rb;
+
+    // variables
+    Vector3 movementVector;
+
+    public enum PlayerStates
+    {
+        Normal,
+        Sneaking
+    }
+
+    public PlayerStates playerState;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerState = PlayerStates.Normal;
+
+    }
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        Vector2 inputVector = context.ReadValue<Vector2>();
+
+        movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+    }
+
+    public void OnSneaking(InputAction.CallbackContext context)
+    {
+        if (playerState == PlayerStates.Normal)
+        {
+            playerState = PlayerStates.Sneaking;
+            moveSpeed /= 2;
+        }
+        else if (playerState == PlayerStates.Sneaking)
+        {
+            playerState = PlayerStates.Normal;
+            moveSpeed *= 2;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = movementVector * moveSpeed;
+    }
+}
