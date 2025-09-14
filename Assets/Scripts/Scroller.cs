578ac8f570fadceee419d8b0a31384efe3df5764
Zhamanta Chavez <zhamantach04@gmail.com>
2025-09-14 17:33:20 -0500

    Scrolling Background Done

--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using System.Runtime.CompilerServices;
 
 public class Scroller : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private RawImage _img;
+    [SerializeField] private float _x, _y;
 
     // Update is called once per frame
     void Update()
     {
-        
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
     }
 }
